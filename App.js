9749a3f73ef85fa0434d4d01def1bd0c1af580e1
Raoul Bumbasi Jr <raoulbumbasi@gmail.com>
2024-01-05 14:44:27 -0800

    edit App.js and home.tsx

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
+import React from "react";
 import { Text, View } from "react-native";
 
 export default function App() {
-  return <Home />;
+  return (
+    <View>
+      <Text>Hello World</Text>
+    </View>
+  );
 }
