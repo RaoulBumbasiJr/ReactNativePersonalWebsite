6ef220d142fd356b833a8d232dde4fb3a21b1d68
Raoul Bumbasi Jr <raoulbumbasi@gmail.com>
2024-01-06 19:05:34 -0800

    added Navigation.tsx

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { Text, View } from "react-native";
+import { Navigation } from "./src/components/Navigation";
 
 export default function App() {
   return (
     <View>
+      <Navigation />
       <Text>Hello World</Text>
     </View>
   );
