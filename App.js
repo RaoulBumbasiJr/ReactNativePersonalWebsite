9909df4136f7b80e5f5c51c472f605fb2d88ce05
Raoul Bumbasi Jr <raoulbumbasi@gmail.com>
2024-01-07 19:09:45 -0800

    added Home.tsx

--- App.js
+++ App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import { Text, View } from "react-native";
 import { Navigation } from "./src/components/Navigation";
+import { Home } from "./src/components/Home";
 
 export default function App() {
   return (
     <View>
       <Navigation />
-      <Text>Hello World</Text>
+      <Home />
     </View>
   );
 }
