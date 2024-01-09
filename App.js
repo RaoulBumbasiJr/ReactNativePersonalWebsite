b63543f7f62d405d3c24156191b2d2f146aafa51
Raoul Bumbasi Jr <raoulbumbasi@gmail.com>
2024-01-08 19:20:07 -0800

    added Portfolio.tsx

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from "react";
 import { Text, View } from "react-native";
 import { Navigation } from "./src/components/Navigation";
 import { Home } from "./src/components/Home";
+import { Portfolio } from "./src/components/Portfolio";
 
 export default function App() {
   return (
     <View>
       <Navigation />
       <Home />
+      <Portfolio />
     </View>
   );
 }
