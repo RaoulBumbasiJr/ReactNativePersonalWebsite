e61cb0af3cc42e1f963d5b51f0543016e7b5ab38
Raoul Bumbasi Jr <raoulbumbasi@gmail.com>
2024-01-12 21:05:11 -0800

    Home.tsx edit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,10 +4,8 @@ import { View, Text } from "native-base";
 export const Home = () => {
   return (
     <View alignItems={"center"} justifyContent={"center"} flex={0} height={100}>
-      <Text>
-        hello, my name is raoul bumbasi jr :) i can code in html, css,
-        javascript, and react native
-      </Text>
+      <Text>hello, my name is raoul bumbasi jr :)</Text>
+      <Text>i can code in html, css, javascript, and react native</Text>
     </View>
   );
 };
