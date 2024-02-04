83606ac1a3b85ad4840a9f7b688b06cc7c6c137a
RaoulBumbasiJr <raoulbumbasi@gmail.com>
2024-02-04 10:20:06 -0800

    Home.tsx edit

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,49 +1,58 @@
 import React from "react";
-import { View, Text, Image, Box } from "native-base";
-import htmlLogo from "../assets/images/htmlLogo.png";
-import cssLogo from "../assets/images/cssLogo.png";
-import jsLogo from "../assets/images/jsLogo.png";
-import reactLogo from "../assets/images/reactLogo.png";
-import wordpressLogo from "../assets/images/wordpressLogo.png";
-import adobeLogo from "../assets/images/adobeLogo.png";
+import {
+  View,
+  Text,
+  Image,
+  Box,
+  NativeBaseProvider,
+  Pressable,
+} from "native-base";
+import { Linking } from "react-native";
 
 export const Home = () => {
+  const handleOpenGitHub = () => {
+    Linking.openURL("https://github.com/RaoulBumbasiJr"); // Replace with your GitHub URL
+  };
+
   return (
-    <View
-      flex={0}
-      position={"relative"}
-      width={"100%"}
-      minH={"60vh"}
-      backgroundColor={"black"}
-      flexDirection={"row"}
-    >
-      <Box
-        backgroundColor={"white"}
-        alignItems={"center"}
-        justifyContent={"center"}
-        alignContent={"center"}
-        flex={1}
+    <NativeBaseProvider>
+      <View
+        flex={0}
+        position={"relative"}
+        width={"100%"}
+        minH={"60vh"}
+        backgroundColor={"black"}
+        flexDirection={"row"}
       >
-        <Text fontSize={50} fontWeight={"bold"} color={"black"}>
-          About me
-        </Text>
-        <Text fontSize={40} fontWeight={"bold"} color={"black"} width={"70%"}>
-          front-end website and mobile development with experience in React
-          Native, Figma, Adobe, and more
-        </Text>
-      </Box>
-      <Box
-        backgroundColor={"gray"}
-        flex={1}
-        alignItems={"center"}
-        justifyContent={"center"}
-        alignContent={"center"}
-      >
-        <Text color={"white"} fontSize={50}>
-          Github link
-        </Text>
-        <Text color={"white"}>https://github.com/RaoulBumbasiJr</Text>
-      </Box>
-    </View>
+        <Box
+          backgroundColor={"white"}
+          alignItems={"center"}
+          justifyContent={"center"}
+          alignContent={"center"}
+          flex={1}
+        >
+          <Text fontSize={50} fontWeight={"bold"} color={"black"}>
+            About me
+          </Text>
+          <Text fontSize={40} fontWeight={"bold"} color={"black"} width={"70%"}>
+            Front-end website and mobile development with experience in React
+            Native, Figma, Adobe, and more
+          </Text>
+        </Box>
+        <Box
+          backgroundColor={"gray"}
+          flex={1}
+          alignItems={"center"}
+          justifyContent={"center"}
+          alignContent={"center"}
+        >
+          <Pressable onPress={handleOpenGitHub}>
+            <Text color={"white"} fontSize={50}>
+              Github link
+            </Text>
+          </Pressable>
+        </Box>
+      </View>
+    </NativeBaseProvider>
   );
 };
