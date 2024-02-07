6caa38ab952e72f21324774f94a048da9d0f12b0
RaoulBumbasiJr <raoulbumbasi@gmail.com>
2024-02-07 11:50:44 -0800

    Portfolio.tsx edit

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { View, Text } from "native-base";
+import { View, Box } from "native-base";
 
 export const Portfolio = () => {
+  const boxes = Array(6).fill({});
+
   return (
     <View
       flex={0}
@@ -10,7 +12,22 @@ export const Portfolio = () => {
       minH={"100vh"}
       backgroundColor={"black"}
     >
-      <Text color={"white"}>Portfolio</Text>
+      {Array.from({ length: 2 }).map((_, rowIndex) => (
+        <View key={rowIndex} flexDirection="row" justifyContent="space-around">
+          {boxes.slice(rowIndex * 3, rowIndex * 3 + 3).map((_, index) => (
+            <Box
+              key={index}
+              borderColor={"pink"}
+              borderWidth={1}
+              width={"30%"}
+              height={"45vh"}
+              backgroundColor={"white"}
+              padding={10}
+              margin={2}
+            />
+          ))}
+        </View>
+      ))}
     </View>
   );
 };
