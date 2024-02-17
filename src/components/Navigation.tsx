44521fe6e8632db4c30e1784b0c2cf7b804174fe
RaoulBumbasiJr <raoulbumbasi@gmail.com>
2024-02-16 21:58:44 -0800

    Edit Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import { View, Text, Box, Button } from "native-base";
+import { View, Text, Box, Button, Pressable, HStack } from "native-base";
 
 export const Navigation = () => {
   return (
     <View backgroundColor={"black"} width={"100%"}>
       <Box>
-        <Text
+        <Pressable
           textAlign={"left"}
           fontSize={50}
           paddingLeft={50}
@@ -13,15 +13,33 @@ export const Navigation = () => {
           paddingTop={50}
         >
           Raoul Bumbasi Jr
-        </Text>
-        <Text
-          textAlign={"right"}
-          color={"white"}
-          fontSize={30}
-          paddingRight={100}
-        >
-          about portfolio contact
-        </Text>
+        </Pressable>
+        <HStack textAlign={"right"}>
+          <Pressable
+            textAlign={"right"}
+            color={"white"}
+            fontSize={30}
+            paddingRight={5}
+          >
+            about
+          </Pressable>
+          <Pressable
+            textAlign={"right"}
+            color={"white"}
+            fontSize={30}
+            paddingRight={5}
+          >
+            about
+          </Pressable>
+          <Pressable
+            textAlign={"right"}
+            color={"white"}
+            fontSize={30}
+            paddingRight={5}
+          >
+            about
+          </Pressable>
+        </HStack>
       </Box>
     </View>
   );
