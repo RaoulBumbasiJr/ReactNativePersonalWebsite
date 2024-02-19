622bb24297b87e73292b6fa548272cc6b8b4f4e9
RaoulBumbasiJr <raoulbumbasi@gmail.com>
2024-02-18 22:27:09 -0800

    Edit Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -3,51 +3,60 @@ import { View, Box, Pressable, HStack, Text } from "native-base";
 
 export const Navigation = () => {
   return (
-    <View backgroundColor={"black"} width={"100%"}>
-      <Box>
+    <View backgroundColor={"#323232"} width={"100%"} justifyContent={"center"}>
+      <HStack justifyContent={"center"} padding={5} alignItems="center">
         <Pressable>
-          <Text
-            textAlign={"left"}
-            fontSize={50}
-            paddingLeft={50}
-            color={"white"}
-          >
+          <Text fontSize={30} paddingRight={50} color={"white"}>
             Raoul Bumbasi Jr
           </Text>
         </Pressable>
-        <HStack justifyContent={"right"}>
-          <Pressable>
-            <Text
-              textAlign={"right"}
-              color={"white"}
-              fontSize={30}
-              paddingRight={5}
-            >
-              home
-            </Text>
-          </Pressable>
-          <Pressable>
-            <Text
-              textAlign={"right"}
-              color={"white"}
-              fontSize={30}
-              paddingRight={5}
-            >
-              work
-            </Text>
-          </Pressable>
-          <Pressable>
-            <Text
-              textAlign={"right"}
-              color={"white"}
-              fontSize={30}
-              paddingRight={5}
-            >
-              contact
-            </Text>
-          </Pressable>
-        </HStack>
-      </Box>
+        <Box
+          width="1px"
+          height="30px"
+          backgroundColor="white"
+          marginX={2}
+          alignSelf="center"
+        />
+
+        <Pressable>
+          <Text color={"white"} fontSize={30} textAlign={"center"}>
+            Home
+          </Text>
+        </Pressable>
+
+        <Box
+          width="1px"
+          height="30px"
+          backgroundColor="white"
+          marginX={2}
+          alignSelf="center"
+        />
+        <Pressable>
+          <Text color={"white"} fontSize={30}>
+            Work
+          </Text>
+        </Pressable>
+
+        <Box
+          width="1px"
+          height="30px"
+          backgroundColor="white"
+          marginX={2}
+          alignSelf="center"
+        />
+        <Pressable>
+          <Text color={"white"} fontSize={30}>
+            Contact
+          </Text>
+        </Pressable>
+        <Box
+          width="1px"
+          height="30px"
+          backgroundColor="white"
+          marginX={2}
+          alignSelf="center"
+        />
+      </HStack>
     </View>
   );
 };
